6192408dcdab8ba05907eb0bc6803cc8577a3e7e
wojtek <krkezc@gmail.com>
2020-09-28 19:05:22 +0200

    moved logic to viewmodel, button animations

--- app/src/main/java/com/example/ebeebequiz/QuizFragment.kt
+++ app/src/main/java/com/example/ebeebequiz/QuizFragment.kt
@@ -3,16 +3,27 @@ package com.example.ebeebequiz
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.DecelerateInterpolator
+import android.widget.RadioButton
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import kotlinx.android.synthetic.main.quiz_fragment.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class QuizFragment : Fragment() {
 
@@ -32,27 +43,78 @@ class QuizFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        var questionPos = 0
+        var cardChangerJob: Job? = null
 
         progressBar.max = quizQuestions.size * 100
-        setProgressAnimate(questionPos)
 
-        showQuestion(quizQuestions[questionPos])
+        viewModel.progress.observe(viewLifecycleOwner, Observer { progress ->
+            setProgressAnimate(progress)
+        })
+
+        viewModel.question.observe(viewLifecycleOwner, Observer { quizQuestion ->
+            if (viewModel.quizPosition == 0) {
+                showQuestion(quizQuestion)
+            } else {
+                card.animate()
+                    .translationX(-view.width.toFloat())
+                    .apply { duration = 150 }
+                    .setListener(object : AnimatorListenerAdapter() {
+                        override fun onAnimationEnd(animation: Animator?) {
+                            showQuestion(quizQuestion)
+                            card.translationX = view.width.toFloat()
+                            card.animate()
+                                .translationX(0f)
+                                .apply { duration = 150 }
+                                .setListener(null)
+                            button.text = "Next question"
+                            button.setBackgroundColor(Color.WHITE)
+                        }
+                    })
+            }
+        })
+
+        viewModel.answerResult.observe(viewLifecycleOwner, Observer { isCorrect ->
+            if (isCorrect) {
+                button.text = "✔"
+            } else {
+                button.text = "✘"
+            }
+            animateButtonOnAnswer(isCorrect)
+
+            cardChangerJob = lifecycleScope.launch(Dispatchers.Main) {
+                delay(750)
+                viewModel.nextQuestion()
+            }
+        })
+
 
         button.setOnClickListener {
-            if (++questionPos >= quizQuestions.size) questionPos = 0
-            setProgressAnimate(questionPos)
-            Log.d("QuizFragment", questionPos.toString())
-            card.animate().translationX(-view.width.toFloat()).apply { duration = 150 }
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator?) {
-                        showQuestion(quizQuestions[questionPos])
-                        card.translationX = view.width.toFloat()
-                        card.animate().translationX(0f).apply { duration = 150 }.setListener(null)
-                    }
-                })
+            cardChangerJob?.cancel()
+            val checkedRadioButtonId = answers.checkedRadioButtonId
+            val checkedButton = answers.findViewById<RadioButton>(checkedRadioButtonId)
+            if (checkedButton != null) {
+                val answer = checkedButton.text.toString()
+                Log.d("QuizFragment", answer)
+                viewModel.buttonClick(answer)
+            } else {
+                Toast.makeText(requireContext(), "You need to select answer", Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
 
+
+    }
+
+    private fun animateButtonOnAnswer(isCorrect: Boolean) {
+        val color = if (isCorrect) {
+            ColorDrawable(Color.GREEN)
+        } else {
+            ColorDrawable(Color.RED)
+        }
+        val colors = arrayOf(button.background, color)
+        val transition = TransitionDrawable(colors)
+        button.background = transition
+        transition.startTransition(200)
     }
 
     private fun setProgressAnimate(questionPos: Int) {
--- app/src/main/java/com/example/ebeebequiz/QuizViewModel.kt
+++ app/src/main/java/com/example/ebeebequiz/QuizViewModel.kt
@@ -1,7 +1,46 @@
 package com.example.ebeebequiz
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class QuizViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    var quizPosition = 0
+
+    private val _progress = MutableLiveData<Int>(quizPosition)
+    val progress: LiveData<Int>
+        get() = _progress
+
+    private val _question = MutableLiveData<QuizQuestion>(quizQuestions[quizPosition])
+    val question: LiveData<QuizQuestion>
+        get() = _question
+
+    private val _answerResult = MutableLiveData<Boolean>()
+    val answerResult: LiveData<Boolean>
+        get() = _answerResult
+
+    var wasAnswerChecked = false
+
+    fun buttonClick(answer: String) {
+        if (wasAnswerChecked) {
+            nextQuestion()
+        } else {
+            _answerResult.value = checkAnswer(answer)
+        }
+    }
+
+    fun nextQuestion() {
+        quizPosition++
+        if (quizPosition == quizQuestions.size) quizPosition = 0
+        wasAnswerChecked = false
+        _progress.value = quizPosition
+        _question.value = quizQuestions[quizPosition]
+    }
+
+    private fun checkAnswer(answer: String): Boolean {
+        wasAnswerChecked = true
+        return quizQuestions[quizPosition].answer == answer
+    }
+
+
 }
